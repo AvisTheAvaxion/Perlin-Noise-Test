b6fd93298d6d49f966715f4b5edb948032d04b0c
AvisTheAvaxion <legodaniele@gmail.com>
2022-02-02 08:05:30 -0500

    moved bot to main.py

--- main.py
+++ main.py
@@ -1,40 +1,71 @@
-from perlin_noise import PerlinNoise
 import random
-import discord_bot
+# import discord_bot
 
-noise = PerlinNoise(octaves=10, seed=1)
-
-w, h = 8, 8
-Matrix = [[0 for x in range(w)] for y in range(h)]
-
-Matrix[0][0] = 1
-Matrix[0][6] = 3
-
-print(Matrix[0][0]) # prints 1
-x, y = 0, 6
-print(Matrix[x][y])
-print("\n\n\n\n")
+# discord_bot.run()
 
 
-def print_array(arr, width, height):
-    s = ""
-    for i in range(width):
-        for i1 in range(height):
-            s = s + str(arr[i][i1])
-
-        print(s)
-        s = ""
+import hikari
+import hidden
+import opensimplex
+import random
+import map_maker
 
 
-print_array(Matrix, w, h)
+def run():
+    bot = hikari.GatewayBot(token=hidden.token)
 
+    @bot.listen()
+    async def ping(event: hikari.GuildMessageCreateEvent) -> None:
+        # If a non-bot user sends a message "hk.ping", respond with "Pong!"
+        # We check there is actually content first, if no message content exists,
+        # we would get `None' here.
 
-for i in range(w):
-    for i1 in range(h):
-        Matrix[i][i1] = random.randint(1, 5)
+        w, h = 12, 12
+        matrix = [[0 for x in range(w)] for y in range(h)]
 
-print_array(Matrix, w, h)
-print("\n\n\n\n")
+        if event.is_bot or not event.content:
+            return
 
+        if event.content.startswith("!genOpen"):
+            opensimplex.seed(random.randint(0, 2000))
+            s = ""
+            for i in range(w):
+                for i1 in range(h):
+                    v = opensimplex.noise2(i, i1) * 10
+                    if v < 0:
+                        v = v * -1
 
-discord_bot.run()
+                    if v > 10:
+                        v = v / 10
+                        v = int(v)
+
+                    v = v + 1
+                    matrix[i][i1] = int(v)
+
+                    s = s + str(matrix[i][i1])
+
+                # await event.message.respond(s)
+                s = ""
+
+            # await event.message.respond("Done!")
+            await event.message.respond(map_maker.arr_to_map(matrix, w, h))
+
+        elif event.content.startswith("!genRand"):
+            s = ""
+            for i in range(w):
+                for i1 in range(h):
+                    matrix[i][i1] = random.randint(1, 5)
+                    s = s + str(matrix[i][i1])
+
+                # await event.message.respond(s)
+                s = ""
+
+            # await event.message.respond("Done!")
+            await event.message.respond(map_maker.arr_to_map(matrix, w, h))
+
+        elif event.content.startswith("!imgTest"):
+            await event.message.respond("🟩🟦🟨⬜⬛⁉")
+
+    bot.run()
+
+run()
--- map_maker.py
+++ map_maker.py
@@ -2,30 +2,30 @@ def arr_to_map(arr, w, h):
     s = ""
     for i in range(w):
         for i1 in range(h):
-            print(arr[i][i1])
+            # print(arr[i][i1])
             if arr[i][i1] == 1 or arr[i][i1] == 2:
                 s = s + "🟦"
-                print("1/2")
+                #  print("1/2")
 
             elif arr[i][i1] == 3 or arr[i][i1] == 4:
                 s = s + "🟨"
-                print("3/4")
+                # print("3/4")
 
             elif arr[i][i1] == 5 or arr[i][i1] == 6:
                 s = s + "🟩"
-                print("5/6")
+                # print("5/6")
 
             elif arr[i][i1] == 7 or arr[i][i1] == 8:
                 s = s + "⬜"
-                print("7/8")
+                # print("7/8")
 
             elif arr[i][i1] == 9 or arr[i][i1] == 10:
                 s = s + "⬜"
-                print("9/10")
+                # print("9/10")
 
             else:
                 s = s + "⁉"
-                print("!?")
+                # print("!?")
 
         s = s + "\n"
 
